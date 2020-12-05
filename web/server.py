2828a5e394d737e352d3862a83e8becb1f14bb4d
wilsonsujames <43058541+wilsonsujames@users.noreply.github.com>
2020-12-05 23:02:59 +0800

    Add files via upload

---
+++ web/server.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=[ "GET",'POST'])
+def index():
+    a={"name":"bob","date":"12月5日","time":"23:30","peopleCount":"10位","phone":"0123456789"}
+
+    Webray=[]
+    Webray.append(a)
+
+    return render_template('index.html',Webray=Webray)
+
+
+
+
+@app.route('/dashboard',methods=[ "GET",'POST'])
+def dashboard():
+    print('dashboard')
+
+    return render_template('dashboard.html')    
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,threaded=True,port=5566)    
